b7a90cbc5d52f531e2c9624f45e57f473b49e7bb
Edwin <zhmail0830@163.com>
2023-06-25 17:34:07 +0800

    fix(gpt-runner-web): Fix base_url issue

--- packages/gpt-runner-web/client/src/helpers/constant.ts
+++ packages/gpt-runner-web/client/src/helpers/constant.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_CHAT_NAME = 'New Chat'
 export const IS_LOCAL_HOST = ['localhost', '127.0.0.1'].includes(window.location.hostname)
-export const BASE_URL = document.querySelector('base')?.href || '/'
+
+const _BASE_URL = document.querySelector('base')?.href || '/'
+export const BASE_URL = _BASE_URL.endsWith('/') ? _BASE_URL.slice(0, -1) : _BASE_URL
