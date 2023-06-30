bc9607e537019fc69aae91fd973380273ed87eaa
JinmingYang <2214962083@qq.com>
2023-06-30 13:54:36 +0800

    feat: support jiti realtime dev

--- alias.ts
+++ alias.ts
@@ -7,10 +7,9 @@ function r(p: string) {
 export const alias: Record<string, string> = {
   '@nicepkg/gpt-runner': r('./packages/gpt-runner/src/'),
   '@nicepkg/gpt-runner-cli': r('./packages/gpt-runner-cli/src/'),
-  '@nicepkg/gpt-runner-config': r('./packages/gpt-runner-config/src/'),
+  '@nicepkg/gpt-runner-core': r('./packages/gpt-runner-core/src/'),
+  '@nicepkg/gpt-runner-shared': r('./packages/gpt-runner-shared/src/'),
+  '@nicepkg/gpt-runner-vscode': r('./packages/gpt-runner-vscode/src/'),
   '@nicepkg/gpt-runner-web/client': r('./packages/gpt-runner-web/client/src/'),
   '@nicepkg/gpt-runner-web/server': r('./packages/gpt-runner-web/server/src/'),
-  '@nicepkg/gpt-runner-shared/browser': r('./packages/gpt-runner-shared/src/browser'),
-  '@nicepkg/gpt-runner-shared/common': r('./packages/gpt-runner-shared/src/common'),
-  '@nicepkg/gpt-runner-shared/node': r('./packages/gpt-runner-shared/src/node'),
 }
